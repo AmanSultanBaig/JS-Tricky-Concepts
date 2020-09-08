c97324272e49f0a1ea5883e5ed589a9d5da84b10
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-09-09 00:52:01 +0500

    things are clear with comments

--- js-tricky-concepts.js
+++ js-tricky-concepts.js
@@ -1,15 +1,17 @@
 // What would be output
 
-
+// declare variables  
 let b;
 var a;
 const c;
 
+// initialize variables with different values
 b = 4;
 a = 3;
 
+// console output
 console.log(a)
 console.log(b)
 
 c = 5;
-console.log(c)
+console.log(c) // Expected Output should be an Error!
